d83ab3d16a48fe549e872d665f911f47ccf48b11
Markus Keppeler <Markus.Keppeler@de.ibm.com>
2021-05-07 17:40:26 +0200

    Updates

--- component---src-pages-404-js-b409b5662aee542361d9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{tz2N:function(e,n,t){"use strict";t.r(n);t("tbR7");var o=t("BZRN"),s=t("GP0y"),r=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];n.default=function(){return Object(s.b)(o.a,{links:r})}}}]);
-//# sourceMappingURL=component---src-pages-404-js-b409b5662aee542361d9.js.map
